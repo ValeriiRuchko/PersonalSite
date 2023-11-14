16a2a60e57220a7e1ad5e8c6da29138551dad2ca
ValeriiRuchko <valerii.ruchko@gmail.com>
2023-11-14 21:33:04 +0100

    Finally completed animation of "About me section"

--- client/src/components/IntroCard.tsx
+++ client/src/components/IntroCard.tsx
@@ -9,7 +9,15 @@ function IntroCard(props: CardProps) {
   return (
     <div className={props.className[0]}>
       <div className={`${props.className[1]}`}>
-        <div className={`${props.className[0]}_title`}>
+        <div
+          className={
+            props.className.length === 2
+              ? `${props.className[0]}_title`
+              : `${props.className[0]}_title ${props.className
+                  .slice(2)
+                  .join(" ")}`
+          }
+        >
           <h2>{props.title}</h2>
         </div>
         <div className={`${props.className[0]}_content`}>
--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import IntroCard from "../../components/IntroCard";
 import "../../styles/App.scss";
 
@@ -8,6 +9,42 @@ type MainPageProps = {
 };
 
 function MainPage(props: MainPageProps) {
+  const [classesLeft, setLeft] = useState([
+    "introduction_section_card",
+    "left_card",
+  ]);
+  const [classesRight, setRight] = useState([
+    "introduction_section_card",
+    "right_card",
+  ]);
+
+  function triggerHover() {
+    console.log(window.scrollY);
+    if (window.scrollY > 200) {
+      setLeft([...classesLeft, "trigger_left"]);
+      setRight([...classesRight, "trigger_right"]);
+    }
+
+    if (window.scrollY > 800 || window.scrollY === 0) {
+      setLeft((prevState) => {
+        return [...prevState, "cancel_left"];
+      });
+      setRight((prevState) => {
+        return [...prevState, "cancel_right"];
+      });
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", triggerHover);
+
+    // cleanup function
+    return () => {
+      window.removeEventListener("scroll", triggerHover);
+      console.log("removed");
+    };
+  }, []);
+
   return (
     <div className={`${props.className}`}>
       <div className="greetings_heading">
@@ -25,7 +62,7 @@ function MainPage(props: MainPageProps) {
           And the clock is ticking. 
           Yeah, but John, if The Pirates of the Caribbean breaks down, 
           the pirates don’t eat the tourists. Is this my espresso machine?`}
-          className={["introduction_section_card", "to_right_hover"]}
+          className={[...classesLeft]}
         />
         <IntroCard
           title="What I do now"
@@ -33,7 +70,7 @@ function MainPage(props: MainPageProps) {
           And the clock is ticking. 
           Yeah, but John, if The Pirates of the Caribbean breaks down, 
           the pirates don’t eat the tourists. Is this my espresso machine?`}
-          className={["introduction_section_card", "to_left_hover"]}
+          className={[...classesRight]}
         />
       </div>
     </div>
